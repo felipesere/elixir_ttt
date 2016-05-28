5d06adc5e9899efa32bd81c949707fe03996b4e1
Felipe Sere <felipesere@gmail.com>
2016-05-28 10:00:38 +0100

    A basic display

--- lib/display.ex
+++ lib/display.ex
@@ -8,9 +8,25 @@ defmodule Display do
     board
   end
 
-  def get_move(_) do
-    3
+  def get_move() do
+    IO.gets("What move do you want to make?\n>")
+    |> String.strip
+    |> validate
+    |> message
   end
+
+  defp validate(move) do
+    case Integer.parse(move) do
+      {n, ""} -> n
+      _ -> {:not_a_number, move}
+    end
+  end
+
+  def message({:not_a_number, move}) do
+    IO.puts("Sorry, '#{move}' is not a number.")
+    get_move
+  end
+  def message(n), do: n
 
   def draw(board) do
     board
--- test/display_test.exs
+++ test/display_test.exs
@@ -1,11 +1,35 @@
 defmodule DisplayTest do
   use ExUnit.Case
-  
+  import ExUnit.CaptureIO
+
   test "dislpays a board properly" do
     board = Board.create
 
     expected = "|1|2|3|\n|4|5|6|\n|7|8|9|"
 
     assert Display.draw(board) == expected
+  end
+
+  test "gets an integer move" do
+    capture_io("2", fn ->
+      send self, Display.get_move
+    end)
+    move = receive do
+      n -> n
+    end
+
+    assert move == 2
+  end
+
+  test "asks again if input is not a number" do
+    output = capture_io("bob\n8", fn ->
+      send self, Display.get_move
+    end)
+    move = receive do
+      n -> n
+    end
+
+    assert move == 8
+    assert String.contains?(output, "Sorry, 'bob' is not a number.")
   end
 end
